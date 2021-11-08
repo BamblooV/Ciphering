99f84310ce2a9b9ddbaf8a0df0191298207a4c67
Egor Gavrilenko <bambloo769@gmail.com>
2021-11-08 20:24:08 +0300

    feat: add main-file

---
+++ ciphering.js
@@ -0,0 +1,62 @@
+const { inputValue: input, outputValue: output, configValue: config} = require('./validateConfig');
+const fs = require('fs');
+const path = require('path');
+const { pipeline } = require('stream');
+const { CesarStream } = require('./Streams/cesarStream.js');
+const { Rot8Stream } = require('./Streams/rot8Stream.js');
+const { AtbahStream } = require('./Streams/atbashStream.js');
+const { ReadStream } = require('./Streams/ReadStream.js');
+const { WriteStream } = require('./Streams/WriteStream.js');
+
+let input_stream;
+if (input) {
+  const existFile = fs.existsSync(path.join(__dirname, input));
+  const accesFile = fs.accessSync(path.join(__dirname, input));
+  if (existFile && !accesFile) {
+    input_stream = new ReadStream(path.join(__dirname, input));
+  } else {
+    process.stderr.write('Input file don`t exist or read-only');
+    process.exit(1);
+  }
+} else {
+  input_stream = process.stdin;
+}
+
+let output_stream;
+if (output) {
+  const existFile = fs.existsSync(path.join(__dirname, output));
+  const accesFile = fs.accessSync(path.join(__dirname, output));
+  if (existFile && !accesFile) {
+    output_stream = new WriteStream(path.join(__dirname, output))
+  } else {
+    process.stderr.write('Output file don`t exist or read-only');
+    process.exit(1);
+  }
+} else {
+  output_stream = process.stdout;
+}
+
+const streams = config.map( element => {
+  if (element[0] === 'C') {
+    return (parseInt(element[1], 10)) ? new CesarStream('encode') : new CesarStream('decode');
+    }
+  if (element[0] === 'R') {
+    return (parseInt(element[1], 10)) ? new Rot8Stream('encode') : new Rot8Stream('decode');
+  }
+  if (element[0] === 'A') {
+    return new AtbahStream();
+  }
+});
+
+pipeline(
+  input_stream,
+  ...streams,
+  output_stream,
+  (error) => {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log('succeed');
+    }
+  }
+)
