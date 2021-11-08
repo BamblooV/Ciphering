19359d966fdd99e73c711c3dcc0faf20a48e6252
Egor Gavrilenko <bambloo769@gmail.com>
2021-11-08 20:23:37 +0300

    feat: add validation for console args

---
+++ validateConfig.js
@@ -0,0 +1,50 @@
+let consoleArgs = process.argv.slice(2);
+const propertyObj = {
+  inputValue: null,
+  outputValue: null,
+  configValue: null
+};
+const aliases = {
+  '--config': '-c',
+  '--input': '-i',
+  '--output': '-o'
+};
+const configMask = ['C1','C0','R1','R0','A'];
+
+consoleArgs = consoleArgs.map(arg => {
+  if (Object.keys(aliases).includes(arg)) {
+    return aliases[arg];
+  } else {
+    return arg;
+  }
+});
+
+const set = new Set(consoleArgs);
+if (set.size !== consoleArgs.length) {
+  process.stderr.write('There are duplicate of console args')
+  process.exit(1)
+};
+
+if (consoleArgs.includes('-c')) {
+  const flagIndex = consoleArgs.indexOf('-c');
+  propertyObj.configValue = consoleArgs[flagIndex + 1].split('-').map(element => {
+    if (!configMask.includes(element)) {
+      process.stderr.write(`There are wrong config args, available: ${configMask}`);
+      process.exit(1);
+    } else {
+      return element;
+    }
+  });;
+};
+
+if (consoleArgs.includes('-i')) {
+  const flagIndex = consoleArgs.indexOf('-i');
+  propertyObj.inputValue = consoleArgs[flagIndex + 1];
+};
+
+if (consoleArgs.includes('-o')) {
+  const flagIndex = consoleArgs.indexOf('-o');
+  propertyObj.outputValue = consoleArgs[flagIndex + 1];
+};
+
+module.exports = propertyObj;
