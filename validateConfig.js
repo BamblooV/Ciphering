df151c63fd51bce1954d97fd83b4aa149b832ef1
Egor Gavrilenko <bambloo769@gmail.com>
2021-11-08 20:40:42 +0300

    fix: check for none args case

--- validateConfig.js
+++ validateConfig.js
@@ -4,12 +4,19 @@ const propertyObj = {
   outputValue: null,
   configValue: null
 };
+
 const aliases = {
   '--config': '-c',
   '--input': '-i',
   '--output': '-o'
 };
+
 const configMask = ['C1','C0','R1','R0','A'];
+
+if (consoleArgs.length == 0) {
+  process.stderr.write('There are no console args');
+  process.exit(1);
+}
 
 consoleArgs = consoleArgs.map(arg => {
   if (Object.keys(aliases).includes(arg)) {
@@ -21,20 +28,26 @@ consoleArgs = consoleArgs.map(arg => {
 
 const set = new Set(consoleArgs);
 if (set.size !== consoleArgs.length) {
-  process.stderr.write('There are duplicate of console args')
-  process.exit(1)
+  process.stderr.write('There are duplicate of console args');
+  process.exit(1);
 };
 
 if (consoleArgs.includes('-c')) {
   const flagIndex = consoleArgs.indexOf('-c');
-  propertyObj.configValue = consoleArgs[flagIndex + 1].split('-').map(element => {
-    if (!configMask.includes(element)) {
-      process.stderr.write(`There are wrong config args, available: ${configMask}`);
-      process.exit(1);
-    } else {
-      return element;
-    }
-  });;
+  try {
+    propertyObj.configValue = consoleArgs[flagIndex + 1].split('-').map(element => {
+      if (!configMask.includes(element)) {
+        process.stderr.write(`There are wrong config args, available: ${configMask}`);
+        process.exit(1);
+      } else {
+        return element;
+      }
+    });
+  } catch (error) {
+    process.stderr.write(`Wrong config args, try 'C1-R1-A'`);
+    process.exit(1);
+  }
+
 };
 
 if (consoleArgs.includes('-i')) {
